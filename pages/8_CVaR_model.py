13638c11fc0f5c6c9bc1b1e8e624231b98728472
ikergomezgarza <ikergogiga@gmail.com>
2026-04-22 13:03:40 +0200

    Update 8_CVaR_model.py

--- pages/8_CVaR_model.py
+++ pages/8_CVaR_model.py
@@ -190,11 +190,11 @@ if st.button("Start the CVaR"):
 
     with col1:
         st.markdown("**Max Sharpe Weights**")
-        st.dataframe(max_sharpe_weights[max_sharpe_weights > 0.001].sort_values(ascending=False).map("{:.1%}".format),hide_index= True)
+        st.dataframe(max_sharpe_weights[max_sharpe_weights > 0.001].sort_values(ascending=False).map("{:.1%}".format) )
 
     with col2:
         st.markdown("**Min CVaR Weights**")
-        st.dataframe(min_cvar_weights[min_cvar_weights > 0.001].sort_values(ascending=False).map("{:.1%}".format),hide_index= True)
+        st.dataframe(min_cvar_weights[min_cvar_weights > 0.001].sort_values(ascending=False).map("{:.1%}".format))
         
 st.write("")
 st.page_link("main.py", label="Back to Home")
